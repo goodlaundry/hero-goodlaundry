44ba3a5331fc224cfd709abb6dfeed688d26b077
goodlaundry <129980328+goodlaundry@users.noreply.github.com>
2025-11-21 16:31:44 -0700

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -217,3 +217,4 @@ export default async function handler(req, res) {
     console.error('Error:', error);
     return res.status(500).json({ error: 'Server error', details: error.message });
   }
+}
